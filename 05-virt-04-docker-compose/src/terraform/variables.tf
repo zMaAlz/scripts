d168df1ba651176a3cf57110055a1f4a45d9b20d
Alexey <yi-7777@rambler.ru>
2022-02-19 23:15:13 +0300

    docker-compous prometeus+grafana

---
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gr7vq3ldsplf604lc4"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g90l6ch8g3isjrnq9c"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd80ag8n59lqpk61gdqq"
+}
